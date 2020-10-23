80a0fa04dfabb601b9c0eb942cb8056b1a384058
poncetnoemie <33011265+poncetnoemie@users.noreply.github.com>
2020-10-23 16:59:57 +0200

    Update alert message

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,21 +1,8 @@
 <template>
   <div>
-    <h2 class="text-red mb-2">Exposition du 24 et 25 octobre 2020 confirmée</h2>
-    <p class="font-bold">
-      Les membres de l'association Place des Arts sont heureux de vous annoncer
-      que l'exposition du 24 et 25 octobre aura bien lieu.
-    </p>
+    <h2 class="text-red mb-2">Annulation de l'exposition Place des Arts <br />du mois d'octobre 2020</h2>
     <p>
-      Aujourd'hui, toutes les conditions sont réunies pour la tenue de notre
-      exposition en respect des consignes Anti-Covid 19 toujours en cours (
-      distanciation, port du masque, gel hydroalcoolique, nombre de personnes
-      restreint sous tonnelle, etc.).
-    </p>
-    <p>
-      Bien sûr, en fonction de l'évolution de la situation sanitaire, nous ne
-      pouvons garantir que cette situation perdure jusqu'aux dates prévues. Mais
-      nous ne manquerons pas d'avertir les artistes par mail et annonce sur site
-      de tout changement susceptible de modifier cette situation.
+      Les membres de l'association Place des Arts ont le regret de vous annoncer <strong>l'annulation de l'exposition de ce weekend (24 & 25 octobre)</strong> du fait d'un arrêté préfectoral interdisant toute exposition contenu de la situation sanitaire.
     </p>
     <p>
       Cordialement,<br />
